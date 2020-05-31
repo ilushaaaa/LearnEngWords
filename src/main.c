fb72042242ed52ed01460b6291d19e88f410f015
Artyom Kolbun <kolbunartema@gmail.com>
2020-05-31 19:11:14 +0700

    Add variable menu

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
+#include "howstring.h"
 #include <locale.h>
 #include <stdio.h>
 int main()
 {
     setlocale(LC_ALL, "Russian");
+    int menu = 0;
     FILE* fEng = fopen("EngWords.txt", "r");
     FILE* fRus = fopen("RusWords.txt", "r");
-    s rewind(fEng);
+    printf("%d", howstring(fEng));
+    rewind(fEng);
     rewind(fRus);
     fclose(fEng);
     fclose(fRus);
+    switch (menu) {
+    case 0: {
+        printf("TEST\n");
+        return 0;
+    }
+    }
     return 0;
 }
